6bfe59b25f84c6fcb54188d40afa4864cd00b509
Sahadev1431 <naisahadev@gmail.com>
2024-07-17 19:23:41 +0530

    connect to database and create routes

--- api/index.js
+++ api/index.js
@@ -1,10 +1,23 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
+
+dotenv.config()
+
+const dbConnection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO)
+        console.log("database connected successfully")
+    } catch (error) {
+        console.log(error)
+    }
+}
+dbConnection() 
 
 const app = express()
 
-app.get("/",(req,res) => {
-    res.send("Heloo")
-})
+app.use("/api/user",userRouter)
 
 app.listen(3000,() => {
     console.log("Server listening on port 3000");
