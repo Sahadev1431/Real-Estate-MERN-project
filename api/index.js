5b71e89e4496483d6ea200b65f3e349da7eff063
Sahadev1431 <naisahadev@gmail.com>
2024-08-01 12:48:36 +0530

    add / route

--- api/index.js
+++ api/index.js
@@ -25,6 +25,9 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
+app.get("/",(req,res) => {
+    res.send("hello")
+})
 app.use("/api/user",userRouter)
 app.use("/api/auth",authRouter)
 app.use("/api/listing",listingRouter)
